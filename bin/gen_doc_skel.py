0115c124b8a9ee4f39174d2e747c7d76972d6765
Brian Luft <brian@unbracketed.com>
2010-07-15 22:33:54 -0700

    updated skel generator to add attribute-specific pages updated README with more info

--- bin/gen_doc_skel.py
+++ bin/gen_doc_skel.py
@@ -1,13 +1,25 @@
+"""
+Introspect Model options to generate a skeleton for the documentation.
+This script depends on being run from within a Django shell.
+"""
+import os
 import re
 
 from django.db import models
 
-class Meta: pass
-Dummy = type('Dummy', (models.Model, ), {'__module__': 'django.db', 'Meta': Meta})
 
+class Meta: pass
+Dummy = type('Dummy', (models.Model, ), {'__module__': 'django.db',
+					 'Meta': Meta})
+
+docs_path = os.path.abspath(os.path.join(__file__, '..', '..', 'docs'))
 
 with open('index.rst', 'w') as f:
 	for attr in [attr for attr in dir(Dummy._meta) if not attr.startswith('_')]:
+		
+		with open(os.path.join(docs_path, '%s.rst' % attr), 'w') as rf:
+			rf.write('TODO documentation and/or example code')
+		
 		if callable(getattr(Dummy._meta, attr)) and getattr(Dummy._meta, attr).__doc__:
 			#strip off newline and replace extraneous spaces
 			doc = re.sub(r'\s{2,}', ' ', getattr(Dummy._meta, attr).__doc__.lstrip())
---
+++ docs/pk_index.rst
@@ -0,0 +1,1 @@
+TODO documentation and/or example code
